17075d9b891fe89ad5bc580f26dddaf47d552e5f
kugelblitz84 <111196826+kugelblitz84@users.noreply.github.com>
2024-09-12 12:52:04 +0600

    Add files via upload
    bitwise or of a given range

---
+++ cont2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#define int long long
+using namespace std;
+void solve(){
+	int n,m;
+	cin>>n>>m;
+	int l=max(0ll,n-m);
+	int r=n+m;
+	int ans=0;
+	int range=r-l+1;
+	for(int i=0;i<=31;i++){
+		bool set_ith=0;
+		if(range>(1<<i)) set_ith=1;
+		else if(l/(1<<i) == r/(1<<i)) {
+			set_ith = ((l/(1<<i))&1);
+		}
+		else {
+			set_ith=1;
+		}
+		ans|=(set_ith<<i);
+	}
+	cout<<ans<<"\n";
+}
+int32_t main() {
+	//memset(dp,-1,sizeof(dp));
+    int t=1;
+    cin>>t;
+    while(t--){
+    	solve();
+    }
+    return 0;
+}
